5e961c3186560f5e59768b8ffebe165fde4bf3d3
Admin1 <Admin1@Admin>
2022-04-01 00:36:01 +0530

    1st commit

---
+++ Applet Demo/src/com/app1/Applet1.java
@@ -0,0 +1,20 @@
+package com.app1;
+
+import java.applet.Applet;
+import java.awt.Button;
+
+public class Applet1 extends Applet{
+
+	 Button b;
+	   public void init()
+	   {
+		   b=new Button("submit");
+		   add(b);
+	   }
+		
+		
+	 }
+	
+	
+	
+	
